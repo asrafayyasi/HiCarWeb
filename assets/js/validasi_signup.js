42df19b23ccc197f2bd8f5a81af5255f5b02e51b
asrafayyasi <ayyasiasraf@gmail.com>
2023-03-30 12:55:56 +0700

    Changes to be committed: 	modified:   assets/js/validasi_signup.js 	modified:   login/loginAdmin.html 	modified:   login/signUpAdmin.html 	modified:   login/signUpCustomer.html

--- assets/js/validasi_signup.js
+++ assets/js/validasi_signup.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-// const password2 = document.getElementById('password2');
+const password2 = document.getElementById('password2');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -37,7 +37,7 @@ const validateInputs = () => {
   const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
-  // const password2Value = password2.value.trim();
+  const password2Value = password2.value.trim();
 
   if (usernameValue === '') {
     setError(username, 'Username is required');
@@ -61,11 +61,11 @@ const validateInputs = () => {
     setSuccess(password);
   }
 
-  // if (password2Value === '') {
-  //   setError(password2, 'Please confirm your password');
-  // } else if (password2Value !== passwordValue) {
-  //   setError(password2, "Passwords doesn't match");
-  // } else {
-  //   setSuccess(password2);
-  // }
+  if (password2Value === '') {
+    setError(password2, 'Please confirm your password');
+  } else if (password2Value !== passwordValue) {
+    setError(password2, "Passwords doesn't match");
+  } else {
+    setSuccess(password2);
+  }
 };
